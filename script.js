cbaa273395337d6db42da963500c4b1048d60b4f
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-15 11:16:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const line = document.getElementById("line");
 let angle = 0;
 function rotateLine(){
-	angle+=2;
-	line.style.transform=`transition(-50%,-50%) rotate($(angle)deg)`
+	angle += 2;
+	line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
 	requestAnimationFrame(rotateLine);
 }
+rotateLine();
